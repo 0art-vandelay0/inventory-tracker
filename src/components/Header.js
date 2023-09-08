f447522a14fbf0c668be5a12fba9fb74a8335208
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-08 10:27:28 -0700

    Add App with Header and CoffeeControl placeholder

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header () {
+    
+    return (
+        <React.Fragment>
+            <div className="header">
+                <h1>Coffee Shop Inventory Tracker</h1>
+                <hr />
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
