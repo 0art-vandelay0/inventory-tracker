fe829ec5c1c9bcf67a5fab9aacf5ef7d96512ad0
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-09-08 13:07:42 -0700

    Add edit onclick to coffee item div

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 function Coffee(props) {
     return (
         <React.Fragment>
-            <div className="coffee">
+            <div className="coffee" onClick={() => props.whenCoffeeClicked(props.id)}>
                 <h3>{props.name}</h3>
                 <p>{props.origin}</p>
                 <p>{props.roast}</p>
@@ -23,7 +23,8 @@ Coffee.propTypes = {
     notes: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
     stock: PropTypes.number.isRequired,
-    id: PropTypes.string
+    id: PropTypes.string,
+    whenCoffeeClicked: PropTypes.func
 };
 
 export default Coffee;
